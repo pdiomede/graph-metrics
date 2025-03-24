3e9b9044b3da3256dd3939d9a00574bca2af53eb
Paolo Diomede <pdiomede@yahoo.com>
2025-03-24 23:01:37 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+// Good for Vercel
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
